2cb28fec875e86a92dcc79b8cd256b762d57a13c
networkdowntime <networkdowntime@gmail.com>
2016-03-31 19:18:55 -0500

    Added lambda expression to test class

--- javaAnalyzer/src/test/java/testClasses/TestClass2.java
+++ javaAnalyzer/src/test/java/testClasses/TestClass2.java
@@ -1,6 +1,11 @@
 package testClasses;
 
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class TestClass2 extends TestClass1 {
 
+	static {
+		Collections.sort(new ArrayList<String>(), (String s1, String s2) -> s1.compareTo(s2));
+	}
 }
