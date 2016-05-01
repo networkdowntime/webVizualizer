04a2a8618717a77a79848286666094a400d0a095
networkdowntime <networkdowntime@gmail.com>
2016-05-01 12:59:00 -0500

    Integrated JavaAnalyzer enhancements plus began plug-in refactoring.
    There were many enhancements done to the JavaAnalyzer in the past 40 or
    so commits merged in from another repo focusing on improving the Java
    features and capabilities.  Some of these broke the DB analyzer.
    Updated the DB Analyzer to work with these changes and also incorporated
    some of UI fixes.

--- webServer/src/main/webapp/js/analyzer.js
+++ webServer/src/main/webapp/js/analyzer.js
@@ -1,14 +1,16 @@
 /**
  * 
  */
+// TODO Move the loading of the plugins to a rest call
+var plugins = [
+    { script: "/dbAnalyzer/js/dbAnalyzer.js", label: "DB Analyzer", id: "dbAnalyzer" }, 
+    { script: "/javaAnalyzer/js/javaAnalyzer.js", label: "Java Analyzer", id: "javaAnalyzer" }
+   ];
+	
+var currentPlugin;
+
 var fake = "";
 //fake = "Fake";
-
-var referenceChainDepths = [ 0, 1, 2, 3, 4, 5, 6 ];
-var layouts = { 'dot' : 'Top Down', 'neato' : 'Natural' };
-var pkFilter = { 'All' : 'All', 'NoPK' : 'No PK', 'HasPK' : 'Has PK' };
-var fkFilter = { 'All' : 'All', 'NoFK' : 'No FK', 'HasFK' : 'Has FK' };
-var javaDiagramType = { 'PACKAGE_DIAGRAM' : 'Package Diagram', 'CLASS_ASSOCIATION_DIAGRAM' : 'Class Diagram', 'UNREFERENCED_CLASSES' : 'Unreferenced Classes' };
 
 $(document).ready(function() {
     $(document)
@@ -22,301 +24,6 @@ $(document).ready(function() {
         $('#loadingDiv').hide();
     });
 });
-
-function javaAnalyzerInit(menuItem) {
-
-	if (!$("#referenceChains").prop('checked')) {
-		$("#referenceChainDetails").hide();
-	}
-	
-	$.each(referenceChainDepths, function(index, value) {
-		$("#upstreamReferenceDepth").append("<option value='"+value+"'>"+value+"</option>");
-		$("#downstreamDependencyDepth").append("<option value='"+value+"'>"+value+"</option>");
-	});
-	
-	$("#referenceChains").change(function() {
-		if ($("#referenceChains").prop('checked')) {
-			$("#referenceChainDetails").show();
-		} else {
-			$("#referenceChainDetails").hide();
-			
-		}
-    	fixSideBarMaxHeight(null, true);
-	});
-	
-	$.each(layouts, function(value, label) {
-		$("#javaLayout").append("<option value='"+value+"'>"+label+"</option>");
-	});
-	
-	$.each(javaDiagramType, function(value, label) {
-		$("#javaDiagramType").append("<option value='"+value+"'>"+label+"</option>");
-	});
-	
-	sourceDir = $.cookie("sourceDir");
-	if (sourceDir != null) $("#sourceDir").val(sourceDir);
-	
-	$("#saveToPng").click(function() {
-		var svg = $("svg")[0].outerHTML;
-		
-		var xhr = new XMLHttpRequest();
-		var url = "/api/code/javaScanner/toPng";
-		var params = "svg=" + encodeURIComponent(svg);
-		xhr.responseType = 'blob';
-		xhr.open("POST", url, true);
-
-		//Send the proper header information along with the request
-		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		xhr.onload = function(e) {
-		  if (this.status == 200) {
-			var blob = new Blob([this.response], {type: 'image/png'});
-			var downloadUrl = URL.createObjectURL(blob);
-			var a = document.createElement("a");
-			a.href = downloadUrl;
-		    a.download = "diagram.png";
-		    document.body.appendChild(a);
-		    a.click();
-		    $(a).remove();
-		  }
-		};
-
-		xhr.send(params);
-	})
-
-	$("#sourceAdd").click(function() {
-		sourceDir = $("#sourceDir").val();
-		$.ajax({
-		    url:'/api/code/javaScanner' + fake + '/file',
-		    type:'POST',
-		    data: { path: sourceDir },
-		    dataType: 'json',
-		    contentType: "application/json",
-		    success:function(res){
-		    	$.cookie("sourceDir", sourceDir);
-		    	loadFiles();
-		    	loadPackages();
-		    	loadClasses();
-//				drawGraph();
-		    },
-		    error:function(res){
-		        alert("Bad things happend! " + res.statusText);
-		        alert("Bad things happend! " + res);
-		    }
-		});
-		
-	});
-	
-	function loadFiles() {
-		container = $("#sourceDiv").next();
-		filesDiv = $("#inputFiles");
-		$(filesDiv).empty();
-		$.get('/api/code/javaScanner' + fake + '/files', function(data) {
-    	    $(container).css('overflow-y', 'hidden');
-    		$(data).each(function() {
-        	    $(container).css('overflow-y', 'hidden');
-    			var html = "<div class='inputFile'>";
-    			html += "<span class='ui-icon ui-icon-circle-minus left add-remove-icon-align sourceDel'/>";
-    			html += "<span class='fileText'><span class='fileLabel'>" + this + "</span></span>";
-    			html += "<div>";
-	    		$(html).appendTo(filesDiv);
-	    	});
-    	    $(container).css('overflow-y', '');
-    		fixSideBarMaxHeight(null, true);
-    		
-    		$(".sourceDel").click(function() {
-    			var sourceDir = $(this).siblings(".fileText, fileLabel").text();
-    			var parent = $(this).parent();
-    			$.ajax({
-    			    url:'/api/code/javaScanner' + fake + '/file' + '?' + $.param({'path': sourceDir}),
-    			    type:'DELETE',
-    			    success:function(res){
-    			    	$(parent).remove().slideUp(250);
-    		    		fixSideBarMaxHeight(null, true);
-    			    	loadPackages();
-    			    	loadClasses();
-//    					drawGraph();
-    			    },
-    			    error:function(res){
-    			        alert("Bad things happend! " + res.statusText);
-    			        alert("Bad things happend! " + res);
-    			    }
-    			});
-    			
-    		});
-    		
-		});
-	}
-	
-	function loadPackages() {
-		container = $("#packagesDiv").next();
-		packagesDiv = $(container).children(".content");
-		$(packagesDiv).empty();
-		$.get('/api/code/javaScanner' + fake + '/packages', function(data) {
-    	    $(container).css('overflow-y', 'hidden');
-
-    		$("<div><input type='button' id='packageCheckAll' value='Uncheck All' /><span class='right'><input id='packageSearch' type='text' placeholder='Package search' value=''/></span></div>").appendTo(packagesDiv);
-
-    		$('#packageCheckAll:button').click(function() {
-//    			console.log(this);
-    			if ($(this).attr("value") === 'Uncheck All') {
-        	        $('.packages:not(:hidden)').prop('checked', false);;
-        	        $(this).val('Check All');            				
-        	    	loadClasses();
-        			drawGraph();
-    			} else {
-        	        $('.packages:not(:hidden)').prop('checked', true);;
-        	        $(this).val('Uncheck All');
-        	    	loadClasses();
-        			drawGraph();
-    			}
-    	    });
-    	    
-    		$('#packageSearch').on('change keyup paste', function() {
-    			packageToSearchFor = $("#packageSearch").val();
-    			$('.packages').parent().hide();
-    			$('.packages').filter(function() {
-    				return $(this).attr('name').toLowerCase().indexOf(packageToSearchFor.toLowerCase()) > -1;
-    			}).parent().show();;
-    		});
-    		
-    		$(data).each(function() {
-	    		$("<div><input class='packages' type='checkbox' name="+this+" value="+this+" checked>" + this + "</div>").appendTo(packagesDiv);
-	    	});
-    	    $(container).css('overflow-y', '');
-    	    
-    		fixSideBarMaxHeight(null, true);
-    	    $(".packages").change(function() {
-    	    	loadClasses();
-//    			drawGraph();
-    	    });
-	    });
-	}
-
-	function loadClasses(animate) {
-		var uncheckedPackages = $.makeArray( $.map($(".packages"), function(i) { if (!$(i).prop('checked')) { return $(i).val(); } }) );
-
-		container = $("#classesDiv").next();
-		classesDiv = $(container).children(".content");
-		$(classesDiv).empty();
-		$.ajax({
-		    url:'/api/code/javaScanner' + fake + '/classes',
-		    type:'POST',
-		    data: JSON.stringify(uncheckedPackages),
-		    dataType: 'json',
-		    contentType: "application/json",
-		    success:function(data) {
-//	    	    $(container).css('overflow-y', 'hidden');
-	    	    
-	    		$("<div><input type='button' id='classCheckAll' value='Uncheck All' /><span class='right'><input id='classSearch' type='text' placeholder='Class search' value=''/></span></div>").appendTo(classesDiv);
-
-	    		$('#classCheckAll:button').click(function() {
-//	    			console.log(this);
-	    			if ($(this).attr("value") === 'Uncheck All') {
-	        	        $('.classes:not(:hidden)').prop('checked', false);;
-	        	        $(this).val('Check All');            				
-	        			drawGraph();
-	    			} else {
-	        	        $('.classes:not(:hidden)').prop('checked', true);;
-	        	        $(this).val('Uncheck All');
-	        			drawGraph();
-	    			}
-	    	    });
-	    	    
-	    		$('#classSearch').on('change keyup paste', function() {
-	    			classToSearchFor = $("#classSearch").val();
-	    			$('.classes').parent().hide();
-	    			$('.classes').filter(function() {
-	    				return $(this).attr('name').toLowerCase().indexOf(classToSearchFor.toLowerCase()) > -1;
-	    			}).parent().show();
-	    		});
-	    		
-		    	$(data).each(function() {
-		    		$("<div><input class='classes' type='checkbox' name="+this+" value="+this+" checked>" + this + "</div>").appendTo(classesDiv);
-		    	});
-
-	    		fixSideBarMaxHeight(null, animate);
-
-//	    		$(container).css('overflow-y', '');
-//	    	    $(".classes").change(function() {
-//	    			drawGraph();
-//	    	    });
-		    },
-		    error:function(res){
-		        alert("Bad thing happend! " + res.statusText);
-		        alert("Bad thing happend! " + res);
-		    }
-		});
-	}
-
-	$("#javaRender").click(function() {
-		drawGraph();
-	});
-		
-	var graphVizFile = null;	
-		
-	function drawGraph() {
-		var uncheckedPackages = $.makeArray( $.map($(".packages"), function(i) { if (!$(i).prop('checked')) { return $(i).val(); } }) );
-		var uncheckedClasses = $.makeArray( $.map($(".classes"), function(i) { if (!$(i).prop('checked')) { return $(i).val(); } }) );
-		
-		var upstreamReferenceDepth = null;
-		var downstreamDependencyDepth = null;
-		
-		if ($("#referenceChains").prop('checked')) {
-			upstreamReferenceDepth = $("#upstreamReferenceDepth option:selected").val();
-			downstreamDependencyDepth = $("#downstreamDependencyDepth option:selected").val();
-		}
-		
-		filter = {
-			advancedSearchQuery : $('#advancedSearchQuery').val(),
-			diagramType : $("#javaDiagramType option:selected").val(), // PACKAGE_DIAGRAM, CLASS_ASSOCIATION_DIAGRAM, METHOD_CALL_DIAGRAM
-			showFields : $("#showFields").prop('checked'), // boolean
-			showMethods : $("#showMethods").prop('checked'), //boolean
-			fromFile : $("#fromFile").prop('checked'), //boolean
-			packagesToExclude : uncheckedPackages, //["CREATED_BY", "UPDATED_BY"], // Set<String>
-			classesToExclude : uncheckedClasses, // NoFK, HasFK, All
-			upstreamReferenceDepth : upstreamReferenceDepth,
-			downstreamDependencyDepth : downstreamDependencyDepth
-		};
-		
-		$.ajax({
-		    url:'/api/code/javaScanner' + fake + '/dot',
-		    type:'POST',
-		    data: JSON.stringify(filter),
-		    dataType: 'text',
-		    contentType: "application/json",
-		    success:function(res){
-			     var dotFile = res;
-			     
-			     var format = "svg"; // dot, plain, svg, xdot
-			     var engine = $("#javaLayout option:selected").val(); // dot, neato
-			     
-			     var result = Viz(dotFile, format, engine);
-			     
-			     //  Make file that can be downloaded later.
-			     var data = new Blob([dotFile], {type: 'text/plain'});
-	    		 if (graphVizFile !== null) {
-      				window.URL.revokeObjectURL(graphVizFile);
-    			 }
-    			 graphVizFile = window.URL.createObjectURL(data);
-			     var link = document.getElementById('saveToGv');
-			     link.href = graphVizFile;
-			     
-			     $('svg').remove();
-			     $("#scrollDiv").after(result);
-			     initializeDragScrollZoom();
-		    },
-		    error:function(res){
-		        alert("Bad thing happend! " + res.statusText);
-		        alert("Bad thing happend! " + res);
-		    }
-		});
-	
-	}
-
-	loadFiles();
-	loadPackages();
-	loadClasses(false);
-
-};
 
 (function($){ // secure $ jQuery alias
 	  
@@ -420,13 +127,6 @@ function javaAnalyzerInit(menuItem) {
 						console.log('isClass', isClass);
 	
 						if (isPackage) { // not adding a context to a package right now
-//						    $(".custom-menu").finish().toggle(100).
-//						    
-//						    // In the right position (the mouse)
-//						    css({
-//						        top: event.pageY + "px",
-//						        left: event.pageX + "px"
-//						    });
 						}
 						
 						if (isClass) {
@@ -484,17 +184,11 @@ function javaAnalyzerInit(menuItem) {
 		});
 	}; //end plugin clickthroughhandler
 
-	})( jQuery ); // confine scope
+})( jQuery ); // confine scope
 
 function analyzer() {
 	
 	// Public Variables
-	this.bar = "bar";
-	
-	// Private Variables
-	var mainMenu = [
-	                { label: "Java Analyzer", id: "javaAnalyzer", initializer: "javaAnalyzerInit" }
-	               ];
 	
 	// Public Functions
 	this.init = function() {
@@ -515,13 +209,14 @@ function analyzer() {
 	    	overlay.css('width', totalWidth - left);
 	    	overlay.css('height', totalHeight - top);
 
-			fixSideBarMaxHeight(null, false);
+			fixSideBarMaxHeight($('#javaAnalyzer'), null, false);
+			fixSideBarMaxHeight($('#dbAnalyzer'), null, false);
 		});
 		
-		$(mainMenu).each(function() {
+		$(plugins).each(function() {
 			// hide the main divs
-//			$("#"+this.id).hide();
-//			$("#"+this.id).zIndex(-1);
+			$("#"+this.id).hide();
+			$("#"+this.id).zIndex(-1);
 			$("#"+this.id).resizable({
 			    handles: 'e', minWidth: 5,
 			    resize: function( event, ui ) {
@@ -547,15 +242,14 @@ function analyzer() {
 			    	overlay.css('height', totalHeight - top);
 			    }
 			});
-//			$("#"+this.id).css({ position: "absolute" });
-			
-			window[this.initializer](this); // calls the initializer function specified in the mainMenu data structure
-//			fixSideBarMaxHeight(null, true);
+
+			$.getScript(this.script, function() {
+			});
 		});
 		
 		
         $.fn.slideFadeToggle = function(speed, easing, callback) {
-        	fixSideBarMaxHeight(this, true);
+        	fixSideBarMaxHeight($('#' + currentPlugin.id), this, true);
             return this.animate({opacity: 'toggle', height: 'toggle'}, speed, easing, callback);
         };
 
@@ -585,7 +279,7 @@ function analyzer() {
 		var maxWidth = 0;
 		
 		// Create the menuItems
-		$(mainMenu).each(function() {
+		$(plugins).each(function() {
 			menuItem = $('<div class="mainMenuItem">' + this.label + '</div>').appendTo("#mainMenu");
 			if ($(menuItem).width() > maxWidth)
 				maxWidth = $(menuItem).width(); 
@@ -608,10 +302,11 @@ function analyzer() {
 			$(menuItem).click(function() {
 				var label = $(this).text();
 
-				$(mainMenu).each(function() {
+				$(plugins).each(function() {
 					if (label == this.label) {
 						$("#"+this.id).show();
 						$("#"+this.id).zIndex(1);
+						currentPlugin = this;
 					} else {
 						$("#"+this.id).hide();
 						$("#"+this.id).zIndex(1);
@@ -634,8 +329,8 @@ function analyzer() {
 		
 		// Show menu items on mouseover
 		$("#mainMenu").mouseover(function() {
-			$(mainMenu).each(function() {
-				$("#"+this.id).zIndex(-1);
+			$(plugins).each(function() {
+//				$("#"+this.id).zIndex(-1);
 			});
 
 			$("#mainMenu").children(".mainMenuItem").each(function( index ) {
@@ -648,7 +343,7 @@ function analyzer() {
 			$("#mainMenu").children(".mainMenuItem").each(function( index ) {
 				$(this).hide();
 			});
-			$(mainMenu).each(function() {
+			$(plugins).each(function() {
 				$("#"+this.id).zIndex(1);
 			});
 		});
--- webServer/src/main/webapp/js/sidebars.js
+++ webServer/src/main/webapp/js/sidebars.js
@@ -2,9 +2,9 @@ function isSameDiv(divChanging, otherDiv) {
 	return divChanging != null && $(otherDiv).attr('id') == divChanging.attr('id');
 };
 
-function openContainerCount(divChanging) {
+function openContainerCount(parentDiv, divChanging) {
 	var openContainers = 0;
-	$('.container').each(function() { // count all open or opening containers
+	$(parentDiv).find('.container').each(function() { // count all open or opening containers
 		var sameDiv = isSameDiv(divChanging, $(this));
 		if ((!sameDiv && $(this).css('display') != 'none') || (sameDiv && $(this).css('display') == 'none')) { 
 			openContainers++;
@@ -13,17 +13,17 @@ function openContainerCount(divChanging) {
 	return openContainers;
 };
 
-function getCurrentHeights() {
+function getCurrentHeights(parentDiv) {
 	var heights = new Object();
-	$('.container').each(function() { // count all open or opening containers
+	$(parentDiv).find('.container').each(function() { // count all open or opening containers
 		heights[$(this).attr('id')] = $(this).outerHeight();
 	});
 	return heights;
 };
 
-function getTotalCollapsibleLabelBarHeight() {
+function getTotalCollapsibleLabelBarHeight(parentDiv) {
 	var height = 0;
-	$('.collapsible').each(function() { height += $(this).outerHeight(); });
+	$(parentDiv).find('.collapsible').each(function() { height += $(this).outerHeight(); });
 	return height;
 };
 
@@ -32,27 +32,27 @@ function isVisible(element) {
 };
 
 
-function fixSideBarMaxHeight(divChanging, animateChange) {
+function fixSideBarMaxHeight(parentDiv, divChanging, animateChange) {
 	
 	var totalWindowHeight = $(window).height();
-	var	totalLabelBarHeight = getTotalCollapsibleLabelBarHeight();
-	var availableHeight = (totalWindowHeight - totalLabelBarHeight - $(".sidebar").offset().top);
-	var oldNaturalHeights = getCurrentHeights();
-	var openContainers = openContainerCount(divChanging);
+	var	totalLabelBarHeight = getTotalCollapsibleLabelBarHeight(parentDiv);
+	var availableHeight = (totalWindowHeight - totalLabelBarHeight - $(parentDiv).offset().top);
+	var oldNaturalHeights = getCurrentHeights(parentDiv);
+	var openContainers = openContainerCount(parentDiv, divChanging);
 
 	var evenHeight = availableHeight / openContainers; // if an even height split
 	
-//	console.log("totalWindowHeight:", totalWindowHeight, "total window height:", $(window).height() , "totalLabelBarHeight:", totalLabelBarHeight);
-//	console.log("availableHeight:", availableHeight, "openContainers:", openContainers, "evenHeight:", evenHeight);
-//	console.log(oldNaturalHeights);
+	console.log("totalWindowHeight:", totalWindowHeight, "total window height:", $(window).height() , "totalLabelBarHeight:", totalLabelBarHeight);
+	console.log("availableHeight:", availableHeight, "openContainers:", openContainers, "evenHeight:", evenHeight);
+	console.log(oldNaturalHeights);
 	
-	$('.container').css('height', ''); // clear any set heights
+	$(parentDiv).find('.container').css('height', ''); // clear any set heights
 
 	var changing = false;
 	var prevEvenHeight = 0;
 	do {
 		changing = false;
-		$('.container').each(function() { // if any divs are less than the even height remove them from the calculations and subtract their height
+		$(parentDiv).find('.container').each(function() { // if any divs are less than the even height remove them from the calculations and subtract their height
 			var sameDiv = isSameDiv(divChanging, $(this));
 			if ((!sameDiv && $(this).is(':visible')) || (sameDiv && $(this).is(':hidden'))) {
 				if ($(this).outerHeight() < evenHeight && $(this).outerHeight() >= prevEvenHeight) {
@@ -61,19 +61,22 @@ function fixSideBarMaxHeight(divChanging, animateChange) {
 					changing = true;
 				} 
 			}
-//			console.log($(this).attr('id'), $(this).outerHeight());
+			console.log($(this).attr('id'), $(this).outerHeight());
 		});
 		prevEvenHeight = evenHeight;
 		evenHeight = availableHeight / openContainers; // calculate the new max height
-//		console.log("adjusted availableHeight:", availableHeight, "adjusted openContainers:", openContainers, "evenHeight: ", evenHeight);
+		console.log("adjusted availableHeight:", availableHeight, "adjusted openContainers:", openContainers, "evenHeight: ", evenHeight);
 	} while (changing && openContainers > 0);
 	
 	var maxHeight2 = availableHeight / openContainers; // calculate the new max height
-//	console.log("adjusted availableHeight:", availableHeight, "adjusted openContainers:", openContainers, "maxHeight2: ", maxHeight2);
+	console.log("adjusted availableHeight:", availableHeight, "adjusted openContainers:", openContainers, "maxHeight2: ", maxHeight2);
 
 
-	$('.container').each(function() { // if the container is open or opening and it's natural height exceeds the new max height set the height 
+	// if the container is open or opening and it's natural height exceeds the new max height set the height
+	$(parentDiv).find('.container').each(function() {  
 		var sameDiv = isSameDiv(divChanging, $(this));
+		
+		console.log('fixing heights', 'div', $(this).attr('id'), 'sameDiv', sameDiv);
 		
 		// start by resetting to the old height
 		if (sameDiv && $(this).is(':visible')) {
@@ -81,27 +84,27 @@ function fixSideBarMaxHeight(divChanging, animateChange) {
 		}
 	
 		if ((!sameDiv && $(this).is(':visible')) || (sameDiv && $(this).is(':hidden'))) {
-//			console.log("outerHeight:", $(this).outerHeight());
+			console.log("outerHeight:", $(this).outerHeight());
 			var outerHeight = $(this).outerHeight();
 			if (outerHeight > maxHeight2) {
 				if (sameDiv) {
 					$(this).css('height', maxHeight2+'px');
 				} else {
 					if (animateChange) {
-//						console.log('animate change1', $(this).attr('id'), oldNaturalHeights[$(this).attr('id')]);
+						console.log('animate change1', $(this).attr('id'), oldNaturalHeights[$(this).attr('id')]);
 						$(this).css('height', oldNaturalHeights[$(this).attr('id')]+'px').animate({ height: maxHeight2 }, 'fast');
 					} else {
-//						console.log('change1', $(this).attr('id'), maxHeight2);
+						console.log('change1', $(this).attr('id'), maxHeight2);
 						$(this).height(maxHeight2).animate({ height: maxHeight2 }, 'fast');;
 					}
 				}
 			} else {
 				if (!sameDiv) {
 					if (animateChange) {
-//						console.log('animate change2', $(this).attr('id'), oldNaturalHeights[$(this).attr('id')]);
+						console.log('animate change2', $(this).attr('id'), oldNaturalHeights[$(this).attr('id')]);
 						$(this).height(outerHeight).animate({ height: outerHeight }, 'fast');
 					} else {				
-//						console.log('change2', $(this).attr('id'), outerHeight);
+						console.log('change2', $(this).attr('id'), outerHeight);
 						$(this).height(outerHeight).animate({ height: outerHeight }, 'fast');
 					}
 				}
@@ -109,5 +112,5 @@ function fixSideBarMaxHeight(divChanging, animateChange) {
 		}
 	});
 	
-//	console.log('final heights', getCurrentHeights());
+	console.log('final heights', getCurrentHeights(parentDiv));
 }
